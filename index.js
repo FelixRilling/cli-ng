e39a84de625335c7ce7a517b1885d6d1cecc2c44
FelixRilling <contact@f-rilling.com>
2017-02-25 15:34:35 +0100

    moved methods to main file

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 "use strict";
 
+const similar = require("similar-strings");
+
 const getAliasedMap = require("./lib/getAliasedMap");
 const addCommandToMap = require("./lib/addCommandToMap");
-const getParsedCommand = require("./lib/getParsedCommand");
-const getHelp = require("./lib/getHelp");
-const getCommand = require("./lib/getCommand");
+const parseInput = require("./lib/parseInput");
+const parseType = require("./lib/parseType");
 
 module.exports = class {
     constructor(commands) {
@@ -15,31 +16,99 @@ module.exports = class {
         Object.keys(commands).forEach(key => {
             _this.map = addCommandToMap(_this.map, key, commands[key]);
         });
+        _this.updateAliased();
+    }
+    updateAliased() {
+        const _this = this;
 
         _this.mapAliased = getAliasedMap(_this.map);
         _this.keysAliased = Array.from(_this.mapAliased.keys());
-    }
-    parse(str) {
-        const _this = this;
-
-        return getParsedCommand(str, _this.mapAliased, _this.keysAliased);
-    }
-    help(commandName) {
-        const _this = this;
-
-        return getHelp(commandName, _this.map, _this.mapAliased, _this.keysAliased);
-    }
-    get(commandName) {
-        const _this = this;
-
-        return getCommand(commandName, _this.mapAliased, _this.keysAliased);
     }
     set(commandName, command) {
         const _this = this;
 
         _this.map = addCommandToMap(_this.map, commandName, command);
+        _this.updateAliased();
+    }
+    get(commandName) {
+        const _this = this;
 
-        _this.mapAliased = getAliasedMap(_this.map);
-        _this.keysAliased = Array.from(_this.mapAliased.keys());
+        if (_this.mapAliased.has(commandName)) {
+            const command = _this.mapAliased.get(commandName);
+
+            return {
+                type: "success",
+                data: {
+                    command: command,
+                    caller: commandName
+                }
+            };
+        } else {
+            const similarKeys = similar(commandName, _this.keysAliased);
+
+            return {
+                type: "error",
+                data: {
+                    err: "missingCommand",
+                    missing: commandName,
+                    similar: similarKeys
+                }
+            };
+        }
+    }
+    getAll() {
+        const _this = this;
+
+        return {
+            type: "success",
+            data: {
+                map: _this.map,
+                mapAliased: _this.mapAliased,
+                keys: _this.keys
+            }
+        };
+    }
+    parse(str) {
+        const _this = this;
+        const parsedInput = parseInput(str);
+        const result = _this.get(parsedInput.name);
+        const command = result.data.command;
+
+        if (result.type === "success") {
+            const args = {};
+            const missingArgs = [];
+
+            command.args.forEach((requestedArg, index) => { //Loop over expected args
+                const suppliedArg = parsedInput.args[index];
+
+                if (suppliedArg) { //If arg exists
+                    const parsedArg = parseType(suppliedArg, requestedArg.type);
+
+                    args[requestedArg.name] = parsedArg;
+                } else { //If arg doesnt exists
+                    if (!requestedArg.required) {
+                        args[requestedArg.name] = requestedArg.default;
+                    } else {
+                        missingArgs.push(requestedArg);
+                    }
+                }
+            });
+
+            if (missingArgs.length > 0) { //returns error when not all args are present
+                return {
+                    type: "error",
+                    data: {
+                        err: "missingArg",
+                        missing: missingArgs
+                    }
+                };
+            } else {
+                result.data.args = args;
+            }
+        }
+
+        console.log(result);
+
+        return result;
     }
 };

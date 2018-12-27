a1c4dd9b62a550709869e5a2afab2032453e3f65
Felix Rilling <felix@f-rilling.com>
2018-12-27 21:10:30 +0100

    Made argumentmap use null for optionals without default value, closes #2.

--- spec/lookup/LookupResolver.spec.ts
+++ spec/lookup/LookupResolver.spec.ts
@@ -1,16 +1,39 @@
-import {IArgument} from "../../src/argument/IArgument";
-import {Clingy} from "../../src/Clingy";
-import {CommandMap} from "../../src/command/CommandMap";
-import {ICommand} from "../../src/command/ICommand";
-import {LookupResolver} from "../../src/lookup/LookupResolver";
-import {ILookupErrorMissingArgs} from "../../src/lookup/result/ILookupErrorMissingArgs";
-import {ILookupErrorNotFound} from "../../src/lookup/result/ILookupErrorNotFound";
-import {ResultType} from "../../src/lookup/result/ILookupResult";
-import {ILookupSuccess} from "../../src/lookup/result/ILookupSuccess";
-import {ArgumentResolving} from "../../src/lookup/ArgumentResolving";
+import { IArgument } from "../../src/argument/IArgument";
+import { Clingy } from "../../src/Clingy";
+import { CommandMap } from "../../src/command/CommandMap";
+import { ICommand } from "../../src/command/ICommand";
+import { LookupResolver } from "../../src/lookup/LookupResolver";
+import { ILookupErrorMissingArgs } from "../../src/lookup/result/ILookupErrorMissingArgs";
+import { ILookupErrorNotFound } from "../../src/lookup/result/ILookupErrorNotFound";
+import { ResultType } from "../../src/lookup/result/ILookupResult";
+import { ILookupSuccess } from "../../src/lookup/result/ILookupSuccess";
+import { ArgumentResolving } from "../../src/lookup/ArgumentResolving";
 
 // noinspection TsLint
-const noopFn = () => {
+const noopFn = () => {};
+
+const createMockCommand = (
+    args: IArgument[] = [],
+    sub: Clingy | null = null
+): ICommand => {
+    return {
+        fn: noopFn,
+        args,
+        alias: [],
+        sub
+    };
+};
+
+const createMockArg = (
+    name: string,
+    required: boolean,
+    defaultValue = "defaultValue"
+): IArgument => {
+    return {
+        name,
+        required,
+        defaultValue
+    };
 };
 
 /**
@@ -29,12 +52,12 @@ describe("LookupResolver", () => {
 
     it("Asserts that LookupResolver#resolve returns an ILookupErrorNotFound for non-existent commands.", () => {
         const commandName = "foo";
+
         const lookupResult = new LookupResolver().resolve(
             new CommandMap(),
             [commandName],
             ArgumentResolving.IGNORE
         );
-
         expect(lookupResult.type).toBe(ResultType.ERROR_NOT_FOUND);
         expect((<ILookupErrorNotFound>lookupResult).missing).toBe(commandName);
         expect(lookupResult.pathUsed).toEqual([commandName]);
@@ -43,20 +66,16 @@ describe("LookupResolver", () => {
 
     it("Asserts that LookupResolver#resolve returns an ILookupErrorMissingArgs when arguments are missing.", () => {
         const commandName = "foo";
-        const argument: IArgument = {name: "bar", required: true};
-        const command: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [argument]
-        };
+        const argument: IArgument = { name: "bar", required: true };
+        const command = createMockCommand([argument]);
         const commandMap = new CommandMap();
         commandMap.set(commandName, command);
+
         const lookupResult = new LookupResolver().resolve(
             commandMap,
             [commandName],
             ArgumentResolving.RESOLVE
         );
-
         expect(lookupResult.type).toBe(ResultType.ERROR_MISSING_ARGUMENT);
         expect((<ILookupErrorMissingArgs>lookupResult).missing).toEqual([
             argument
@@ -65,21 +84,17 @@ describe("LookupResolver", () => {
         expect(lookupResult.pathDangling).toEqual([]);
     });
 
-    it("Asserts that LookupResolver#resolve returns the Command.", () => {
+    it("Asserts that LookupResolver#resolve returns the command.", () => {
         const commandName = "foo";
-        const command: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command = createMockCommand();
         const commandMap = new CommandMap();
         commandMap.set(commandName, command);
+
         const lookupResult = new LookupResolver().resolve(
             commandMap,
             [commandName],
             ArgumentResolving.IGNORE
         );
-
         expect(lookupResult.type).toBe(ResultType.SUCCESS);
         expect((<ILookupSuccess>lookupResult).command).toBe(command);
         expect(lookupResult.pathUsed).toEqual([commandName]);
@@ -90,43 +105,37 @@ describe("LookupResolver", () => {
         const commandName = "foo";
         const argumentName = "lorem";
         const argumentVal = "bar";
-        const argument: IArgument = {name: argumentName, required: true};
-        const command: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [argument]
-        };
+        const argument = createMockArg(argumentName, true);
+        const command = createMockCommand([argument]);
         const commandMap = new CommandMap();
         commandMap.set(commandName, command);
+
         const lookupResult = new LookupResolver().resolve(
             commandMap,
-            [commandName, argumentVal, "fizz"],
+            [commandName, argumentVal],
             ArgumentResolving.RESOLVE
         );
-
         expect(lookupResult.type).toBe(ResultType.SUCCESS);
         expect((<ILookupSuccess>lookupResult).command).toBe(command);
         expect((<ILookupSuccess>lookupResult).args).toEqual(
             new Map([[argumentName, argumentVal]])
         );
+        expect(lookupResult.pathUsed).toEqual([commandName]);
+        expect(lookupResult.pathDangling).toEqual([argumentVal]);
     });
 
     it("Asserts that LookupResolver#resolve returns dangling path elements.", () => {
         const commandName = "foo";
         const commandNames = [commandName, "bar", "fizz"];
-        const command: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command = createMockCommand();
         const commandMap = new CommandMap();
         commandMap.set(commandName, command);
+
         const lookupResult = new LookupResolver().resolve(
             commandMap,
             commandNames,
             ArgumentResolving.IGNORE
         );
-
         expect(lookupResult.type).toBe(ResultType.SUCCESS);
         expect((<ILookupSuccess>lookupResult).command).toBe(command);
         expect(lookupResult.pathUsed).toEqual([commandName]);
@@ -135,11 +144,7 @@ describe("LookupResolver", () => {
 
     it("Asserts that LookupResolver#resolve honors caseSensitive.", () => {
         const commandName = "foo";
-        const command: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command = createMockCommand();
         const commandMap = new CommandMap();
         commandMap.set(commandName, command);
 
@@ -162,22 +167,13 @@ describe("LookupResolver", () => {
     });
     it("Asserts that LookupResolver#resolve resolves sub-commands.", () => {
         const commandName2 = "bar";
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command2 = createMockCommand();
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [],
-            sub: clingy
-        };
+        const command1 = createMockCommand([], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -188,28 +184,21 @@ describe("LookupResolver", () => {
         );
         expect(lookupResult.type).toBe(ResultType.SUCCESS);
         expect((<ILookupSuccess>lookupResult).command).toBe(command2);
+        expect(lookupResult.pathUsed).toEqual([commandName1, commandName2]);
+        expect(lookupResult.pathDangling).toEqual([]);
     });
 
     it("Asserts that LookupResolver#resolve resolves sub-commands arguments", () => {
         const commandName2 = "bar";
         const argumentName = "baa";
-        const argument: IArgument = {name: argumentName, required: true};
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [argument]
-        };
+        const argument = createMockArg(argumentName, true);
+        const command2 = createMockCommand([argument]);
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [],
-            sub: clingy
-        };
+        const command1 = createMockCommand([], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -224,28 +213,21 @@ describe("LookupResolver", () => {
         expect((<ILookupSuccess>lookupResult).args).toEqual(
             new Map([[argumentName, argumentVal]])
         );
+        expect(lookupResult.pathUsed).toEqual([commandName1, commandName2]);
+        expect(lookupResult.pathDangling).toEqual([argumentVal]);
     });
 
     it("Asserts that LookupResolver#resolve resolves sub-commands optional arguments", () => {
         const commandName2 = "bar";
         const argumentName = "baa";
-        const argument: IArgument = {name: argumentName, required: false};
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [argument]
-        };
+        const argument = createMockArg(argumentName, false);
+        const command2 = createMockCommand([argument]);
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [],
-            sub: clingy
-        };
+        const command1 = createMockCommand([], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -260,28 +242,49 @@ describe("LookupResolver", () => {
         expect((<ILookupSuccess>lookupResult).args).toEqual(
             new Map([[argumentName, argumentVal]])
         );
+        expect(lookupResult.pathUsed).toEqual([commandName1, commandName2]);
+        expect(lookupResult.pathDangling).toEqual([argumentVal]);
+    });
+
+    xit("Asserts that LookupResolver#resolve correctly errors for missing sub-command arguments", () => {
+        const commandName2 = "bar";
+        const argumentName = "baa";
+        const argument = createMockArg(argumentName, true);
+        const command2 = createMockCommand([argument]);
+        const commandMap2 = new CommandMap();
+        commandMap2.set(commandName2, command2);
+        const clingy = new Clingy(commandMap2);
+
+        const commandName1 = "foo";
+        const command1 = createMockCommand([], clingy);
+        const commandMap1 = new CommandMap();
+        commandMap1.set(commandName1, command1);
+
+        const lookupResult = new LookupResolver().resolve(
+            commandMap1,
+            [commandName1, commandName2],
+            ArgumentResolving.RESOLVE
+        );
+
+        expect(lookupResult.type).toBe(ResultType.ERROR_MISSING_ARGUMENT);
+        expect((<ILookupErrorMissingArgs>lookupResult).missing).toEqual([
+            argument
+        ]);
+        expect(lookupResult.pathUsed).toEqual([commandName1, commandName2]);
+        expect(lookupResult.pathDangling).toEqual([]);
     });
 
     it("Asserts that LookupResolver#resolve resolves sub-commands optional arguments which are not present", () => {
         const commandName2 = "bar";
         const argumentName = "baa";
-        const argument: IArgument = {name: argumentName, required: false};
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [argument]
-        };
+        const argument = createMockArg(argumentName, false);
+        const command2 = createMockCommand([argument]);
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [],
-            sub: clingy
-        };
+        const command1 = createMockCommand([], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -292,30 +295,21 @@ describe("LookupResolver", () => {
         );
         expect(lookupResult.type).toBe(ResultType.SUCCESS);
         expect((<ILookupSuccess>lookupResult).command).toBe(command2);
+        expect(lookupResult.pathUsed).toEqual([commandName1, commandName2]);
+        expect(lookupResult.pathDangling).toEqual([]);
     });
 
     it("Asserts that LookupResolver#resolve resolves sub-commands over arguments when matching", () => {
         const commandName2 = "bar";
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command2 = createMockCommand();
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
-        const arg1 = {
-            name: "arg1",
-            required: true
-        };
+        const argumentName = "argument";
+        const argument = createMockArg(argumentName, true);
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [arg1],
-            sub: clingy
-        };
+        const command1 = createMockCommand([argument], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -326,30 +320,21 @@ describe("LookupResolver", () => {
         );
         expect(lookupResult.type).toBe(ResultType.SUCCESS);
         expect((<ILookupSuccess>lookupResult).command).toBe(command2);
+        expect(lookupResult.pathUsed).toEqual([commandName1, commandName2]);
+        expect(lookupResult.pathDangling).toEqual([]);
     });
 
     it("Asserts that LookupResolver#resolve resolves sub-commands over optional arguments when matching", () => {
         const commandName2 = "bar";
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command2 = createMockCommand();
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
-        const arg1 = {
-            name: "arg1",
-            required: false
-        };
+        const argumentName = "argument";
+        const argument = createMockArg(argumentName, false);
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [arg1],
-            sub: clingy
-        };
+        const command1 = createMockCommand([argument], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -360,31 +345,21 @@ describe("LookupResolver", () => {
         );
         expect(lookupResult.type).toBe(ResultType.SUCCESS);
         expect((<ILookupSuccess>lookupResult).command).toBe(command2);
+        expect(lookupResult.pathUsed).toEqual([commandName1, commandName2]);
+        expect(lookupResult.pathDangling).toEqual([]);
     });
 
     it("Asserts that LookupResolver#resolve resolves arguments over sub-commands when not matching", () => {
         const commandName2 = "bar";
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command2 = createMockCommand();
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
         const argumentName = "arg1";
-        const arg1 = {
-            name: argumentName,
-            required: true
-        };
+        const argument = createMockArg(argumentName, true);
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [arg1],
-            sub: clingy
-        };
+        const command1 = createMockCommand([argument], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -398,31 +373,21 @@ describe("LookupResolver", () => {
         expect((<ILookupSuccess>lookupResult).args).toEqual(
             new Map([[argumentName, argumentName]])
         );
+        expect(lookupResult.pathUsed).toEqual([commandName1, argumentName]);
+        expect(lookupResult.pathDangling).toEqual([argumentName]);
     });
 
     it("Asserts that LookupResolver#resolve resolves arguments over sub-commands when not matching optionals", () => {
         const commandName2 = "bar";
-        const command2: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: []
-        };
+        const command2 = createMockCommand();
         const commandMap2 = new CommandMap();
         commandMap2.set(commandName2, command2);
         const clingy = new Clingy(commandMap2);
 
         const argumentName = "arg1";
-        const arg1 = {
-            name: argumentName,
-            required: false
-        };
+        const argument = createMockArg(argumentName, false);
         const commandName1 = "foo";
-        const command1: ICommand = {
-            fn: noopFn,
-            alias: [],
-            args: [arg1],
-            sub: clingy
-        };
+        const command1 = createMockCommand([argument], clingy);
         const commandMap1 = new CommandMap();
         commandMap1.set(commandName1, command1);
 
@@ -436,5 +401,7 @@ describe("LookupResolver", () => {
         expect((<ILookupSuccess>lookupResult).args).toEqual(
             new Map([[argumentName, argumentName]])
         );
+        expect(lookupResult.pathUsed).toEqual([commandName1, argumentName]);
+        expect(lookupResult.pathDangling).toEqual([argumentName]);
     });
 });
